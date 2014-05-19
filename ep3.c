d1bf20ca9bb519d6da67c64d8341e289aec41087
julio.seki <julio.seki@lge.com>
2014-05-19 17:51:38 -0300

    Melhoria na impressao

--- ep3.c
+++ ep3.c
@@ -6,15 +6,13 @@
 #define TRUE 1
 #define FALSE 0
 
-FILE *leEntrada (char *nomeArquivo);
-
 int main(int argc, char **argv)
 {
     int *vetor;
     int i, cont=0;
-	char carac;
-	FILE *entrada;
-    vetor = malloc(128*sizeof(int));
+	char carac, c;
+	FILE *entrada = NULL;
+    vetor = malloc(127*sizeof(int)); /*existem 127 caracteres na tabela ASCII*/
 	
 	if(argc - 1 > 0)
 	{
@@ -34,8 +32,16 @@ int main(int argc, char **argv)
 	
 	    for(i=1; i<=128; i++)
     {
-        if(vetor[i] != 0)
-            printf("Valor do indice %d: %d\n",i, vetor[i]); /*arrumar indice*/
+		if(i == 10 && vetor[i] != 0)
+		{
+			printf("Numero de ocorrencias do caracter NEW_LINE: %d\n", vetor[i]);
+		}
+		else
+        if(vetor[i] != 0)	
+		{		
+			c = i;
+            printf("Numero de ocorrencias do caracter %c: %d\n",c, vetor[i]);
+		}
     }
     return 0;
 }
