fd363fe291839a5fbfb1577aef9ed2f095b43884
julio.seki <julio.seki@lge.com>
2014-05-28 16:26:54 -0300

    Algumas modificacoes

--- Struct.c
+++ Struct.c
@@ -9,9 +9,19 @@ Celula *criaListaCelula(int *vetor)
 	int i; 
 	char c;
 	Celula *cabCelula = malloc(sizeof(Celula));
+	Celula *aux;
 	Celula *nova = malloc(sizeof(Celula));
+	Celula *aux1 = malloc(sizeof(Celula));
+	Celula *aux2 = malloc(sizeof(Celula));
 	cabCelula->ocorrencias = 42;
-	for(i=1; i<=60; i++)
+	cabCelula->carac = 'A';
+	aux1->ocorrencias = 43;
+	aux1->carac = 'B';
+	aux2->ocorrencias = 44;	
+	aux2->carac = 'C';
+	cabCelula->prox = aux1;
+	cabCelula->prox->prox = aux2;
+	for(i=1; i<=127; i++)
     {
 		/*if(i == 10 && vetor[i] != 0)
 		{
@@ -28,6 +38,7 @@ Celula *criaListaCelula(int *vetor)
 			c = i;
 			nova->carac = c;
 			nova->ocorrencias = vetor[i];
+			nova->prox = NULL;
 			usleep(15000);/*1500000*/
 			if(cabCelula->prox == NULL) /*lista vazia*/
 			{
@@ -36,22 +47,25 @@ Celula *criaListaCelula(int *vetor)
 				printf("Celula inserida: %c, %d\n", nova->carac, nova->ocorrencias);
 			}
 			else
-	    	{
-		        Celula *aux;
-		        aux = cabCelula->prox;
+	    	{		        
+		        aux = cabCelula->prox; /*aux deve estar com carac = 'B'*/
+				printf("ELSE\n");
 		        while(aux->prox != NULL)
 		        {
-		            aux = aux->prox;
-		            printf("PROXIMO\n");
+					printf("WHILE\n");
+					printf("Dados da celula atual: %c %d\n",aux->carac, aux->ocorrencias);
+		            aux = aux->prox;		           
 		        }
 		        aux->prox = nova;
 		        printf("Celula inserida: %c, %d\n", nova->carac, nova->ocorrencias);
     		}
+			printf("saiu do else\n");
 			printf("**************************************************************\n");
 			imprimeCelula(cabCelula);
 			printf("**************************************************************\n");
 		}
 	}
+
 	return cabCelula;
 }
 
